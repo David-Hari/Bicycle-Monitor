2d25fbc25021035237d350a8417e86513851fc32
David <d-hari@hotmail.com>
2019-05-06 20:24:32 +1000

    More work on status messages.

--- display.py
+++ display.py
@@ -5,7 +5,7 @@
 """
 
 from threading import Timer
-from recordclass import recordclass  # pip3 install recordclass
+from recordclass import recordclass
 from PIL import Image, ImageDraw, ImageFont
 import picamera
 
@@ -17,14 +17,14 @@ StatusOverlay = recordclass('StatusOverlay', 'overlay timer yPos')
 fontPath = '/usr/share/fonts/truetype/roboto/Roboto-Regular.ttf'
 statusFont = ImageFont.truetype(fontPath, 30)
 powerBarFont = ImageFont.truetype(fontPath, 30)
-statusOverlays = {}
-statusIdCounter = 0
 statusBackgroundColour = (20,20,20,128)
 statusColours = {
 	'info': (255,255,255),
-	'warning': (255,255,0),
+	'warning': (255,128,0),
 	'error': (255,0,0)
 }
+statusOverlays = {}
+statusIdCounter = 0
 powerBarOverlay = None
 
 camera = picamera.PiCamera()
@@ -43,14 +43,21 @@ def start():
 
 def showStatusText(text, timeout=10, level='info'):
 	"""
-	Draws `text` near the bottom of the screen, then hides it after `timeout` seconds.
+	Draws status text near the bottom of the screen.
+	Any existing status overlays still on screen will be pushed up.
+	:param text: The text to show
+	:param timeout: Text will be hidden after this many seconds
+	:param level: Status level. One of: 'info', 'warning' or 'error'.
+	:return: Id of the status that has been added to the screen
 	"""
 	global statusOverlays
+	global statusIdCounter
 	global statusBackgroundColour
 	global statusColours
 
 	print(text)  # For debugging/logging purposes
 
+	# TODO: Most of this will be done by makeStatusTextImage
 	image = Image.new('RGBA', (config.videoDisplayResolution[0], 160))
 	draw = ImageDraw.Draw(image)
 	width, height = draw.textsize(text, font=statusFont)
@@ -60,37 +67,49 @@ def showStatusText(text, timeout=10, level='info'):
 	draw.rectangle([x,0,x+width,height], fill=statusBackgroundColour)
 	draw.text((x+30,20), text, font=statusFont, fill=statusColours[level])
 	y = config.videoDisplayResolution[1] - image.height
-	
-	makeStatusTextImage(text, bg=statusBackgroundColour, fg=statusColours[level])
-	
+
+	image = makeStatusTextImage(text, bg=statusBackgroundColour, fg=statusColours[level])
+
 	status = StatusOverlay(
-		overlay = addOverlay(image, (0, y, image.width, image.height))
-		timer = Timer(timeout, hideStatusText, args=id)
+		overlay = addOverlay(image, (0, y, image.width, image.height)),
+		timer = Timer(timeout, hideStatusText, args=statusIdCounter),
 		yPos = y
 	)
 	statusOverlays[statusIdCounter] = status
 	statusIdCounter = statusIdCounter + 1
 	status.timer.start()
 
-def updateStatusText(id, text, timeout=10, level='info'):
 
-	print(text)  # For debugging/logging purposes
-	
-	if id is None or id not in statusOverlays:
-		return showStatusText(text, timeout, level)
-	
-	existingStatus = statusOverlays[id]
-	
-
-
-def hideStatusText(id):
+def updateStatusText(statusId, text, timeout=10, level='info'):
 	"""
-	Hides the status text overlay.
+	Updates an existing status overlay with new text.
+	If the overlay does not exist, a new one will be created.
+	:param statusId: Id of the existing status
+	:param text: The new text to show
+	:param timeout: Text will be hidden after this many seconds
+	:param level: Status level if new overlay. One of: 'info', 'warning' or 'error'.
+	:return: The id of the new or existing status
+	"""
+	print(text)  # For debugging/logging purposes
+
+	if statusId is None or statusId not in statusOverlays:
+		return showStatusText(text, timeout, level)
+
+	existingStatus = statusOverlays[statusId]
+	# draw new text
+
+	return statusId
+
+
+def hideStatusText(statusId):
+	"""
+	Hides a status text overlay.
+	:param statusId: Id of the status to hide
 	"""
 	global statusOverlays
-	if id in statusOverlays:
-		camera.remove_overlay(statusOverlays[id].overlay)
-		del statusOverlays[id]
+	if statusId in statusOverlays:
+		camera.remove_overlay(statusOverlays[statusId].overlay)
+		del statusOverlays[statusId]
 
 
 def drawPowerBar(power, goalPower, powerRange, idealRange):
@@ -102,6 +121,8 @@ def drawPowerBar(power, goalPower, powerRange, idealRange):
 	:param idealRange: The high and low bounds of the green (good) area, in Watts
 	:return:
 	"""
+	global powerBarOverlay
+
 	spacing = 20  # Leave room for text at the top and bottom
 	barHeight = powerBarOverlay.window[3] - (spacing * 2)
 	image = Image.new('RGBA', powerBarOverlay.window[2:4])
@@ -141,9 +162,12 @@ def makeStatusTextImage(text, bg, fg):
 	"""
 	Creates an image and draws the given text to it.
 	"""
+	global statusFont
+
+	# TODO keep just the stuff that's needed here
 	image = Image.new('RGBA', (config.videoDisplayResolution[0], 160))
 	draw = ImageDraw.Draw(image)
-	width, height = draw.textsize(text, font)
+	width, height = draw.textsize(text, font=statusFont)
 	width += 60  # Add some padding
 	height += 40
 	x = (config.videoDisplayResolution[0] - width) // 2
