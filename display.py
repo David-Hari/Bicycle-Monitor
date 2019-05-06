37f6417cc49c00d5efe9683541c2641cbbd504df
David Hari <david.hari@objective.com>
2019-05-06 11:55:08 +1000

    Work on multiple status overlays. *** Remember to merge with unsaved stuff at home ***

--- display.py
+++ display.py
@@ -5,17 +5,26 @@
 """
 
 from threading import Timer
+from recordclass import recordclass  # pip3 install recordclass
 from PIL import Image, ImageDraw, ImageFont
 import picamera
 
 import config
 
 
+StatusOverlay = recordclass('StatusOverlay', 'overlay timer yPos')
+
 fontPath = '/usr/share/fonts/truetype/roboto/Roboto-Regular.ttf'
 statusFont = ImageFont.truetype(fontPath, 30)
 powerBarFont = ImageFont.truetype(fontPath, 30)
-statusTimer = None
-statusOverlay = None
+statusOverlays = {}
+statusIdCounter = 0
+statusBackgroundColour = (20,20,20,128)
+statusColours = {
+	'info': (255,255,255),
+	'warning': (255,255,0),
+	'error': (255,0,0)
+}
 powerBarOverlay = None
 
 camera = picamera.PiCamera()
@@ -32,28 +41,15 @@ def start():
 	powerBarOverlay = addOverlay(Image.new('RGBA', (320, 240)), (20, 20, 320, 240))
 
 
-def addOverlay(image, position):
-	"""
-	Adds a region of the screen that can be drawn to.
-	:param image: Initial image to draw
-	:param position: x,y position of the top-left corner of the overlay
-	:return: The overlay object
-	"""
-	assert position[2] % 32 == 0 and position[3] % 16 == 0
-	return camera.add_overlay(image.tobytes(), layer = 3, format = 'rgba', size = image.size, fullscreen = False, window = position)
-
-
-def showStatusText(text, timeout=10, status='info'):
+def showStatusText(text, timeout=10, level='info'):
 	"""
 	Draws `text` near the bottom of the screen, then hides it after `timeout` seconds.
 	"""
-	global statusTimer
-	global statusOverlay
+	global statusOverlays
+	global statusBackgroundColour
+	global statusColours
 
 	print(text)  # For debugging/logging purposes
-
-	if statusTimer is not None:
-		statusTimer.cancel()
 
 	image = Image.new('RGBA', (config.videoDisplayResolution[0], 160))
 	draw = ImageDraw.Draw(image)
@@ -61,24 +57,40 @@ def showStatusText(text, timeout=10, status='info'):
 	width += 60  # Add some padding
 	height += 40
 	x = (config.videoDisplayResolution[0] - width) // 2
-	draw.rectangle([x,0,x+width,height], fill=(20,20,20,128))
-	draw.text((x+30,20), text, font=statusFont, fill=(255,255,255))
-
-	hideStatusText()  # Make sure any previous text is removed
+	draw.rectangle([x,0,x+width,height], fill=statusBackgroundColour)
+	draw.text((x+30,20), text, font=statusFont, fill=statusColours[level])
 	y = config.videoDisplayResolution[1] - image.height
-	statusOverlay = addOverlay(image, (0, y, image.width, image.height))
-	statusTimer = Timer(timeout, hideStatusText)
-	statusTimer.start()
+	
+	makeStatusTextImage(text, bg=statusBackgroundColour, fg=statusColours[level])
+	
+	status = StatusOverlay(
+		overlay = addOverlay(image, (0, y, image.width, image.height))
+		timer = Timer(timeout, hideStatusText, args=id)
+		yPos = y
+	)
+	statusOverlays[statusIdCounter] = status
+	statusIdCounter = statusIdCounter + 1
+	status.timer.start()
+
+def updateStatusText(id, text, timeout=10, level='info'):
+
+	print(text)  # For debugging/logging purposes
+	
+	if id is None or id not in statusOverlays:
+		return showStatusText(text, timeout, level)
+	
+	existingStatus = statusOverlays[id]
+	
 
 
-def hideStatusText():
+def hideStatusText(id):
 	"""
 	Hides the status text overlay.
 	"""
-	global statusOverlay
-	if statusOverlay is not None:
-		camera.remove_overlay(statusOverlay)
-		statusOverlay = None
+	global statusOverlays
+	if id in statusOverlays:
+		camera.remove_overlay(statusOverlays[id].overlay)
+		del statusOverlays[id]
 
 
 def drawPowerBar(power, goalPower, powerRange, idealRange):
@@ -104,11 +116,42 @@ def drawPowerBar(power, goalPower, powerRange, idealRange):
 	draw.rectangle([60,mid,140,y], fill=colour)
 	drawShadowedText(draw, (5,mid-16), str(goalPower), font=powerBarFont, fill=(255,255,255))
 	drawShadowedText(draw, (145,y-16), str(power), font=powerBarFont, fill=(255,255,255))
-	powerBarOverlay.update(image.tobytes())
+	updateOverlay(powerBarOverlay, image)
 
 
 
 ### Private methods ###
+
+def addOverlay(image, position):
+	"""
+	Adds a region of the screen that can be drawn to.
+	:param image: Initial image to draw
+	:param position: x,y position of the top-left corner of the overlay
+	:return: The overlay object
+	"""
+	assert position[2] % 32 == 0 and position[3] % 16 == 0
+	return camera.add_overlay(image.tobytes(), layer = 3, format = 'rgba', size = image.size, fullscreen = False, window = position)
+
+
+def updateOverlay(overlay, image):
+	overlay.update(image.tobytes())
+
+
+def makeStatusTextImage(text, bg, fg):
+	"""
+	Creates an image and draws the given text to it.
+	"""
+	image = Image.new('RGBA', (config.videoDisplayResolution[0], 160))
+	draw = ImageDraw.Draw(image)
+	width, height = draw.textsize(text, font)
+	width += 60  # Add some padding
+	height += 40
+	x = (config.videoDisplayResolution[0] - width) // 2
+	draw.rectangle([x,0,x+width,height], fill=bg)
+	drawShadowedText(draw, (x+30,20), text, font=statusFont, fill=fg)
+	y = config.videoDisplayResolution[1] - image.height
+	return image
+
 
 def drawShadowedText(draw, position, text, font, fill=(255,255,255), shadow=(0,0,0)):
 	x = position[0]
