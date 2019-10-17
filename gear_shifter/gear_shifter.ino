1445a1b140543618be51e1f69ca914378d62584a
David <d-hari@hotmail.com>
2019-10-17 21:05:50 +1100

    Changed from interrupts to polling button state. It works as long as the delay in the main loop is long enough.

--- gear_shifter/gear_shifter.ino
+++ gear_shifter/gear_shifter.ino
@@ -1,39 +1,67 @@
 #include <Servo.h>
 
 
-const int GEAR_UP_PIN = 2;
-const int GEAR_DOWN_PIN = 3;
+const int UP_BUTTON_PIN = 2;
+const int DOWN_BUTTON_PIN = 3;
+const int SERVO_PIN = 4;
+const unsigned long lastDebounceTime = 0;  // Last time a button was pushed (input went LOW)
+const unsigned long debounceDelay = 100;   // Milliseconds delay before detecting another push
 
+int previousUpButtonState = HIGH;
+int previousDownButtonState = HIGH;
+int currentGear = 0;
 Servo servo;
-unsigned long lastDebounceTime = 0;  // Last time a button was pushed (input went LOW)
-unsigned long debounceDelay = 100;   // Milliseconds delay before detecting another push
-//boolean wasGearUpButtonPressed = false;   // To remember the event from an interrupt
-//boolean wasGearDownButtonPressed = false; // ditto
-volatile int currentGear = 0;
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(GEAR_UP_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(GEAR_UP_PIN), gearUpButtonPressed, FALLING);
-  //servo.attach(4);
+  pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
+  //pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+  //servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  Serial.println(currentGear);
-  delay(10);
-}
-
-void gearUpButtonPressed() {
-  // NOTE: This works for button down, but button up also bounces and can generate press events
-  unsigned long currentTime = millis();
-  if ((currentTime - lastDebounceTime) > debounceDelay) {
+  if (checkUpButton()) {
     currentGear++;
-    lastDebounceTime = currentTime;
+    Serial.println(currentGear);
+    blinkLED();
   }
+  else if (checkDownButton()) {
+    currentGear--;
+    Serial.println(currentGear);
+    blinkLED();
+  }
+  delay(100);
 }
 
-void gearDownButtonPressed() {
-  currentGear--;
+// Returns true if the button has been pressed
+boolean checkButton(const int pin, int &previousState) {
+  int currentState = digitalRead(pin);
+
+  // Check if button was pressed
+  if (previousState == HIGH && currentState == LOW) {
+    previousState = currentState;
+    return true;
+  }
+  // Detect release as well (to maintain the correct state)
+  if (previousState == LOW && currentState == HIGH) {
+    previousState = currentState;
+  }
+
+  return false;
+}
+
+boolean checkUpButton() {
+  return checkButton(UP_BUTTON_PIN, previousUpButtonState);
+}
+
+boolean checkDownButton() {
+  return checkButton(DOWN_BUTTON_PIN, previousDownButtonState);
+}
+
+void blinkLED() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
 }
