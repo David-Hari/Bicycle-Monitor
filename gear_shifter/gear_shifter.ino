a56e3d21b753a78edffee4715e63884b486e2659
David <d-hari@hotmail.com>
2019-10-18 20:23:19 +1100

    Re-organised the code a bit.

--- gear_shifter/gear_shifter.ino
+++ gear_shifter/gear_shifter.ino
@@ -12,56 +12,94 @@ int previousDownButtonState = HIGH;
 int currentGear = 0;
 Servo servo;
 
-// The setup function runs once when you press reset or power the board
+
+/*************************************************************************/
+/* The setup function runs once when you press reset or power the board. */
+/*************************************************************************/
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
-  //pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
-  Serial.begin(9600);
-  //servo.attach(SERVO_PIN);
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
+	//pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
+	Serial.begin(9600);
+	//servo.attach(SERVO_PIN);
 }
 
+/*************************************************************************/
+/* The main loop runs continuously.                                      */
+/*************************************************************************/
 void loop() {
-  if (checkUpButton()) {
-    currentGear++;
-    Serial.println(currentGear);
-    blinkLED();
-  }
-  else if (checkDownButton()) {
-    currentGear--;
-    Serial.println(currentGear);
-    blinkLED();
-  }
-  delay(100);
+	waitForInput();
+	sendUpdate();
+	moveServo();
 }
 
-// Returns true if the button has been pressed
+
+/*************************************************************************/
+/* Continuously check button state waiting for a press. Once pressed,    */
+/* increment/decrement gear number and return.                           */
+/*************************************************************************/
+void waitForInput() {
+	while(true) {
+		if (checkUpButton()) {
+			currentGear++;
+			return;
+		}
+		else if (checkDownButton()) {
+			currentGear--;
+			return;
+		}
+		// NOTE: Delay is just a busy loop, so we might as well read the input
+		// continuously without delay. But then we'd have to de-bounce it.
+		delay(100);
+	}
+}
+
+/*************************************************************************/
+/* Send the current gear number over serial communication.               */
+/*************************************************************************/
+void sendUpdate() {
+	Serial.println(currentGear);
+}
+
+/*************************************************************************/
+/* Move the servo motor to the current gear position.                    */
+/*************************************************************************/
+void moveServo() {
+}
+
+
+/*************************************************************************/
+/* Return true if the button has been pressed.                           */
+/*************************************************************************/
 boolean checkButton(const int pin, int &previousState) {
-  int currentState = digitalRead(pin);
+	int currentState = digitalRead(pin);
 
-  // Check if button was pressed
-  if (previousState == HIGH && currentState == LOW) {
-    previousState = currentState;
-    return true;
-  }
-  // Detect release as well (to maintain the correct state)
-  if (previousState == LOW && currentState == HIGH) {
-    previousState = currentState;
-  }
+	// Check if button was pressed
+	if (previousState == HIGH && currentState == LOW) {
+		previousState = currentState;
+		return true;
+	}
+	// Detect release as well (to maintain the correct state)
+	if (previousState == LOW && currentState == HIGH) {
+		previousState = currentState;
+	}
 
-  return false;
+	return false;
 }
 
 boolean checkUpButton() {
-  return checkButton(UP_BUTTON_PIN, previousUpButtonState);
+	return checkButton(UP_BUTTON_PIN, previousUpButtonState);
 }
 
 boolean checkDownButton() {
-  return checkButton(DOWN_BUTTON_PIN, previousDownButtonState);
+	return checkButton(DOWN_BUTTON_PIN, previousDownButtonState);
 }
 
-void blinkLED() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
+
+void turnOnLED() {
+	digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void turnOffLED() {
+	digitalWrite(LED_BUILTIN, LOW);
 }
